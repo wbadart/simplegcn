879da33ae4f239b8990b88209e41b7231490b079
Will Badart <badart_william@bah.com>
2018-10-09 12:14:58 -0400

    Took requirements parsing out of setup.py Turns out the version pinning format is a little different. Plus, makes it more simple to let all the dependencies to be specified once (in the conda environment).

--- setup.py
+++ setup.py
@@ -10,17 +10,9 @@ created: OCT 2018
 """
 
 from setuptools import find_packages, setup
-from yaml import load as load_yml
 
-
-PATH_CONDA_ENV = './.conda.env.yml'
-
-with open(PATH_CONDA_ENV) as fs:
-    REQUIREMENTS = load_yml(fs)['dependencies']
 
 setup(name='simplegcn',
       version='0.0.1-alpha',
       packages=find_packages(),
-
-      package_data={'': ['*.md']},
-      install_requires=REQUIREMENTS)
+      package_data={'': ['*.md']})
