ff13324018398e720f6deac7892a8785ec3e2f97
Will Badart <badart_william@bah.com>
2018-10-29 10:54:38 -0400

    Created simplegcn package Currently just includes a short description, and states goals.

---
+++ simplegcn/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+simplegcn/__init__.py
+
+`simplegcn' aims to provide a barebones implementation of a Graph Convolutional
+network and its components, with minimal dependencies (hopefully only on
+packages already present in a typical data science environment).
+
+Will Badart
+created: OCT 2018
+"""
